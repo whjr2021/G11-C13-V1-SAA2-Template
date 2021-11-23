532e9d0fd44b54c271e6e8f253e70d680b485632
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-23 11:12:10 +0530

    Add files via upload

---
+++ C13_SAA2_Template.py
@@ -0,0 +1,14 @@
+# Define a basic calculator function with 3 inputs - num1, num2 and operator. Name it as "calculator" 
+# The operator values are: +, -, *, /
+
+
+
+
+
+
+
+
+
+
+
+# Call the "calculator" function by passing inputs and print the result function returns
